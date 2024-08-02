860dff528dc1a65a4bc4a5583b93132814ddaead
Tade77 <adekunletaofeeq@mail.com>
2024-08-02 15:47:04 +0100

    Components folder added

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
-import { useState } from "react";
 import "./App.css";
-import { motion } from "framer-motion";
 
 const App = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  return (
-    <motion.div
-      layout
-      data-expanded={isOpen}
-      initial={{ borderRadius: 50 }}
-      className="flex h-[100px] w-[100px] items-center justify-center bg-black data-[expanded=true]:h-[200px] data-[expanded=true]:w-[400px]"
-      onClick={() => setIsOpen(!isOpen)}
-    >
-      <motion.div layout className="h-10 w-10 rounded-full bg-white" />
-    </motion.div>
-  );
+  return <div></div>;
 };
 
 export default App;
